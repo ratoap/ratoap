e87cbaa869defdcb5cfb2267a4338731458fa3b3
vkill <vkill.net@gmail.com>
2016-05-31 12:06:27 +0800

    edit redis script

---
+++ lib/ratoap/redis_scripts/ratoap_get_connect_identity.lua
@@ -0,0 +1,13 @@
+local part_type = ARGV[1]
+local part_name = ARGV[2]
+local token = ARGV[3]
+
+
+math.randomseed(tonumber(string.sub(tostring({}), 8,-1),16))
+
+if part_type == 'driver' and part_name == 'vagrant_ruby' and token == 'xxx' then
+  local str = string.format("%s%s%s%s", part_type, part_name, token, math.random())
+  return redis.sha1hex(str)
+else
+  return 0
+end
