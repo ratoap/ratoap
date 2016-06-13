805a36448d71e1008d94ae167b2e5e354c779a5d
vkill <vkill.net@gmail.com>
2016-06-13 15:44:40 +0800

    edit redis publish message

--- lib/ratoap.rb
+++ lib/ratoap.rb
@@ -61,17 +61,22 @@ module Ratoap
       client_names << "provisioner_#{provisioner['name']}"
     end
 
+    redis.set "ratoap:client_conn:wait", client_names.to_json
+
     i = 0
-    while i <= 10 do
+
+    r = while i <= 10 do
       i += 1
-      client_names.each do |client_name|
-        n = redis.publish("ratoap:client_conn", JSON.dump({name: client_name}))
 
-        if n > 0
-        end
-      end
+      wait_conn_client_names = JSON.parse(redis.get("ratoap:client_conn:wait"))
+      break 1 if wait_conn_client_names.size == 0
 
+      redis.publish("ratoap:client_conn", JSON.dump({act: :wait, redis_scripts: RedisScript.data}))
       sleep 3
+    end
+
+    if r != 1
+      redis.publish("ratoap:client_conn", JSON.dump({act: :quit}))
     end
 
     Process.kill("HUP", client_driver_vagrant_process_pid)
--- lib/ratoap/redis_scripts/ratoap_get_connect_identity.lua
+++ lib/ratoap/redis_scripts/ratoap_get_connect_identity.lua
@@ -3,7 +3,7 @@ local part_name = ARGV[2]
 local token = ARGV[3]
 
 
-math.randomseed(tonumber(string.sub(tostring({}), 8,-1),16))
+math.randomseed(tonumber(string.sub(tostring({}), 8, -1), 16))
 
 if part_type == 'driver' and part_name == 'vagrant_ruby' and token == 'xxx' then
   local str = string.format("%s%s%s%s", part_type, part_name, token, math.random())
